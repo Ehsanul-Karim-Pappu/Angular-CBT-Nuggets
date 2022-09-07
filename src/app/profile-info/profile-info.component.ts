97ea7b29517561263bb2d6d9769900c8455bee7d
Ehsanul-Karim-Pappu <ehsan.pappu.99@gmail.com>
2022-09-07 16:27:26 +0600

    introuduce Inputs to component

--- src/app/profile-info/profile-info.component.ts
+++ src/app/profile-info/profile-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-profile-info',
@@ -6,12 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-info.component.css']
 })
 export class ProfileInfoComponent implements OnInit {
-  name = 'Khandaker Ehsanul Karim';
-  profilePicPath = 'assets/passport%20size%20photo%202.jpg';
-  age = 26;
-  bio = 'I like Angular';
-  birthday = 'March 3';
-  interests = ['Programming', 'VLSI', 'Robotics', 'Reading'];
+  @Input() name = '';
+  @Input() profilePicPath = '';
+  @Input() age = '';
+  @Input() bio = '';
+  @Input() birthday = '';
+  @Input() interests = '';
 
   constructor() { }
 
